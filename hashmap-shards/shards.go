3e9a93e81776050bf54dfa4b84dd23299d52660a
Paulus Robin <paulus.robin@hotmail.com>
2020-06-21 17:38:21 +0700

    move interfaces redis hashmap to shards.go

--- hashmap-shards/shards.go
+++ hashmap-shards/shards.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"hash/fnv"
+	"time"
 )
 
 type (
+	RedisHashMap interface {
+		HMSetWithExpiration(key string, value map[string]interface{}, ttl time.Duration) error
+		HMSet(key string, value map[string]interface{}) error
+		HSetWithExpiration(key, field string, value interface{}, ttl time.Duration) error
+		HSet(key, field string, value interface{}) error
+		HMGet(key string, fields ...string) ([]interface{}, error)
+		HGetAll(key string) (map[string]string, error)
+	}
 	implementation struct {
 		redis  RedisHashMap
 		log    Log
